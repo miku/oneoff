32824eb1eb56c072f66e55a0e45b8fd13dc0a3e8
Martin Czygan <martin.czygan@gmail.com>
2023-12-11 22:43:02 +0100

    fix uppsala

--- cmd/uppsala/main.go
+++ cmd/uppsala/main.go
@@ -3,10 +3,12 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/url"
 	"os"
 	"runtime"
+	"strings"
 
 	"github.com/miku/parallel"
 )
@@ -14,14 +16,19 @@ import (
 var (
 	numWorkers = flag.Int("w", runtime.NumCPU(), "number of workers")
 	batchSize  = flag.Int("b", 20000, "batch size")
+	debug      = flag.Bool("v", false, "output skipped URLs to stderr")
 )
 
 func main() {
 	flag.Parse()
 	pp := parallel.NewProcessor(os.Stdin, os.Stdout, func(p []byte) ([]byte, error) {
+		s := strings.TrimSpace(string(p))
 		_, err := url.Parse(string(p))
-		if err != nil {
+		if err == nil {
 			return p, nil
+		}
+		if *debug {
+			fmt.Fprintf(os.Stderr, "failed: %s\n", s)
 		}
 		return nil, nil
 	})
